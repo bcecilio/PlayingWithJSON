64ad113cbefea423ad63c8770aae849afcd4304b
brendon cecilio <brendon.cecilio@gmail.com>
2020-07-28 12:24:35 -0400

    successfully parsed contacts dictionary

--- Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
+++ Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+let json = """
+{
+    "results": [
+    {
+        "firstName": "John",
+        "lastName": "Appleseed"
+        },
+    {
+        "firstName": "Alex",
+        "lastName": "Paul"
+        }
+    ]
+}
+""".data(using: .utf8)!
 
-//: [Next](@next)
+// Codable -> Encodable + Decodable
+// Decodable: converts json to data
+// Encodable: converts json to data to POST to web API
+
+struct ResultWrapper: Codable {
+    let results: [Contact]
+}
+
+struct Contact: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+do {
+    let dictionay = try JSONDecoder().decode(ResultWrapper.self, from: json)
+    let contacts = dictionay.results // array of contacts
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
